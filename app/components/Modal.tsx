a4fe15eaa8569062a6184867d247aabde8d4d8a3
Piotr Turski <pipolito2115@gmial.com>
2022-11-16 22:43:38 +0100

    add framer motion

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -3,6 +3,8 @@
 import styles from './Modal.module.scss';
 import { useState } from 'react';
 
+import { motion, useMotionValue, useTransform } from 'framer-motion';
+import useMeasure from 'react-use-measure';
 const Modal = () => {
   const [url, setUrl] = useState('');
   const raceHandler = async (event: { preventDefault: () => void }) => {
@@ -17,11 +19,44 @@ const Modal = () => {
     });
   };
 
+  const [ref, bounds] = useMeasure();
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+  const rotateX = useTransform(mouseY, [-250, 250], [-10, 10]);
+  const rotateY = useTransform(mouseX, [-165, 165], [10, -10]);
   return (
     <div className={styles.wrapper}>
-      <div className={styles.container}>
+      <motion.div
+        className={styles.container}
+        ref={ref}
+        style={{ rotateX, rotateY, z: 100 }}
+        onPointerMove={e => {
+          mouseX.set(e.clientX - bounds.x - bounds.width / 2);
+          console.log('x', e.clientX - bounds.x - bounds.width / 2);
+          mouseY.set(e.clientY - bounds.y - bounds.height / 2);
+          console.log('y', e.clientY - bounds.y - bounds.height / 2);
+        }}
+        onHoverEnd={() => {
+          console.log('hover end');
+          mouseX.set(0);
+          mouseY.set(0);
+        }}
+        animate={{
+          gradientTransform: ['rotate(0deg)', 'rotate(360deg)'],
+        }}
+      >
         <h1 className={styles.title}>
-          <span className={styles.title}>🏁</span>
+          <motion.span
+            style={
+              {
+                // rotateX,
+                // rotateY,
+              }
+            }
+            className={styles.title}
+          >
+            🏁
+          </motion.span>
           Put ur seat bels on
         </h1>
         <form onSubmit={raceHandler}>
@@ -33,7 +68,7 @@ const Modal = () => {
           <span>AND</span>
           <button type="submit"> race </button>
         </form>
-      </div>
+      </motion.div>
     </div>
   );
 };
